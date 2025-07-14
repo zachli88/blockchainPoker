58bc23e7939e31cd81107f81fac13362c89bfeed
zachli88 <lizach739@gmail.com>
2025-07-13 21:00:33 -0400

    networking

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 import (
-	"fmt"
-	"github.com/zachli88/blockchainPoker/deck"
+	// "fmt"
+	// "github.com/zachli88/blockchainPoker/deck"
+	"github.com/zachli88/blockchainPoker/p2p"
 )
 
 func main() {
-	for i := 0; i < 10; i++ {
-		d := deck.New()
-		fmt.Println(d)
-		fmt.Println()
+	cfg := p2p.ServerConfig{
+		Version: "1",
+		ListenAddr: "127.0.0.1:3000",
 	}
+	server := p2p.NewServer(cfg)
+	go server.Start()
+
+	remoteCfg := p2p.ServerConfig{
+		Version: "1",
+		ListenAddr: "127.0.0.1:4000",
+	}
+	remoteServer := p2p.NewServer(remoteCfg)
+	go remoteServer.Start()
+	if err := remoteServer.Connect("127.0.0.1:3000"); err != nil {
+		panic(err)
+	}
+
+	select{}
 }
